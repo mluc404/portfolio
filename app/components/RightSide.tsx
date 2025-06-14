b91a0b7339c4c94feca4f11a825624e36cd1d06b
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-06-13 22:27:44 -0700

    style: add sticky-blur effect for section headings

--- app/components/RightSide.tsx
+++ app/components/RightSide.tsx
@@ -6,23 +6,22 @@ export default function RightSide() {
   return (
     <div className="flex flex-col px-6 py-8 gap-8 md:gap-20 md:px-20 md:py-24 ">
       {/* <div className="h-[100vh]"> */}
-      <div id="about" className="flex flex-col gap-4">
-        <div>ABOUT</div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quas eos,
-        obcaecati animi temporibus dolor ea facere maxime necessitatibus
-        perspiciatis nam, tenetur voluptate exercitationem corrupti cupiditate
-        voluptatem itaque repellendus sapiente.
+      <div id="about" className="flex flex-col gap-4 relative">
+        <div className="blur-heading">ABOUT</div> Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus quas eos, obcaecati animi temporibus
+        dolor ea facere maxime necessitatibus perspiciatis nam, tenetur
+        voluptate exercitationem corrupti cupiditate voluptatem itaque
+        repellendus sapiente.
       </div>
-      <div id="projects" className="flex flex-col gap-4">
-        <div>PROJECTS</div>
-
+      <div id="projects" className="flex flex-col gap-4 relative">
+        <div className="blur-heading">PROJECTS</div>
         {myInfo.projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
       </div>
 
-      <div id="experience" className="flex flex-col gap-4">
-        <div>EXPERIENCE</div>
+      <div id="experience" className="flex flex-col gap-4 relative">
+        <div className="blur-heading">EXPERIENCE</div>{" "}
         {myInfo.job.map((job, index) => (
           <JobCard job={job} key={index} />
         ))}
